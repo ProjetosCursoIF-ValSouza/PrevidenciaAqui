0509cc801faa479af531281dc89e982dfa01e5b9
Valdemir <souza.valdemir@aluno.ifsp.edu.br>
2023-06-05 18:05:46 -0300

    alterações

--- routes/simule.js
+++ routes/simule.js
@@ -59,7 +59,7 @@ router.post('/simule', async function (req, res, next) {
 }); 
 
 module.exports = router; */
-
+/*
 const express = require('express');
 const router = express.Router();
 const db = require('../config/database.config'); // Importe o arquivo de configuração do banco de dados
@@ -119,5 +119,146 @@ router.post('/simule', async function (req, res, next) {
   // Verifique se a inserção foi bem-sucedida e retorne uma resposta adequada ao cliente
 });
 
+module.exports = router; */
+/*
+const express = require('express');
+const router = express.Router();
+const db = require('../config/database.config'); // Importe o arquivo de configuração do banco de dados
+
+// Rota para exibir o formulário de simulação
+router.get('/simule', function (req, res, next) {
+  res.render('simule');
+});
+
+// Rota para criar uma nova simulação
+router.post('/simule', async function (req, res, next) {
+  //// Extrair os dados do corpo da solicitação (formulário)
+  let { mesAno, salario, genero, dataNascimento } = req.body;
+
+  genero = genero == "Feminino" ? "f" : "m";
+  const idadeAposentadoria = req.body.genero === 'Masculino' ? 65 : 62;
+  const anoNascimento = new Date(req.body.dataNascimento).getFullYear();
+  const idade = new Date().getFullYear() - anoNascimento;
+  const tempo_contribuicao_mes = req.body['campoAnoMes[]'];
+  const salarios = req.body['campoSalario[]'];
+
+  console.log("DADOS QUE ESTÃO VINDO DO FORM VIA INPUT NAME=VALUE:");
+  console.log(idadeAposentadoria);
+  console.log(anoNascimento);
+  console.log(idade);
+  console.log(tempo_contribuicao_mes.length);
+  console.log(req.body);
+
+  const sql_simulacao = `INSERT INTO simulacao_beneficio (genero, data_nascimento, idade, tempo_contribuicao_mes, idade_aposentadoria) VALUES (?, ?, ?, ?, ?);`;
+  let values = [genero, dataNascimento, idade, tempo_contribuicao_mes.length, idadeAposentadoria];
+  let [result] = await db.poolSimule.query(sql_simulacao, values);
+  const id_simulacao = result.insertId;
+
+  for (let index = 0; tempo_contribuicao_mes && index < tempo_contribuicao_mes.length; index++) {
+    const salario = salarios[index].replace(".", "").replace("R$", "").replace(",", ".");
+    
+    // Validar se o salário é um número válido
+    if (isNaN(salario)) {
+      // Salário inválido, faça algo, como retornar uma mensagem de erro ou interromper o loop
+      console.log(`O salário "${salarios[index]}" é inválido.`);
+      continue;
+    }
+
+    // Validar se o ano e mês são válidos
+    const ano = tempo_contribuicao_mes[index].substring(0, 4);
+    const mes = tempo_contribuicao_mes[index].substring(5, 7);
+    // Faça as validações necessárias para o ano e mês
+
+    // Inserir o registro no banco de dados somente se passar nas validações
+    const query = `INSERT INTO simulacao_periodo_trabalho (ano, mes, salario, simulacao_beneficio_id) VALUES (?, ?, ?, ?)`;
+    const values = [ano, mes, salario, id_simulacao];
+    [result] = await db.poolSimule.query(query, values);
+  }
+
+  res.send();
+
+  // Verifique se a inserção foi bem-sucedida e retorne uma resposta adequada ao cliente
+});
+
+module.exports = router; */
+
+const express = require('express');
+const router = express.Router();
+const db = require('../config/database.config');
+
+// Rota GET para renderizar a página de simulação
+router.get('/simule', function (req, res, next) {
+  res.render('simule');
+});
+
+// Rota POST para processar os dados do formulário de simulação
+router.post('/simule', async function (req, res, next) {
+  try {
+    let { mesAno, salario, genero, dataNascimento } = req.body;
+
+    genero = genero == "Feminino" ? "f" : "m";
+    const idadeAposentadoria = req.body.genero === 'Masculino' ? 65 : 62;
+    const anoNascimento = new Date(req.body.dataNascimento).getFullYear();
+    const idade = new Date().getFullYear() - anoNascimento;
+    const tempo_contribuicao_mes = req.body['campoAnoMes[]'];
+    const salarios = req.body['campoSalario[]'];
+
+    console.log("DADOS QUE ESTÃO VINDO DO FORM VIA INPUT NAME=VALUE:");
+    console.log(idadeAposentadoria);
+    console.log(anoNascimento);
+    console.log(idade);
+    console.log(tempo_contribuicao_mes && tempo_contribuicao_mes.length);
+    console.log(req.body);
+
+    // Função auxiliar para validar o formato do salário (neste caso, se é numérico)
+    function validarSalario(salario) {
+      return !isNaN(parseFloat(salario)) && isFinite(salario);
+    }
+
+    // Validação dos campos do formulário
+    if (!mesAno || !salario) {
+      throw new Error('Por favor, preencha todos os campos de mês/ano e salário.');
+    }
+
+    for (let index = 0; tempo_contribuicao_mes && index < tempo_contribuicao_mes.length; index++) {
+      const salario = salarios[index].replace(".", "").replace("R$", "").replace(",", ".");
+      
+      if (!salario || !validarSalario(salario)) {
+        throw new Error(`O salário "${salarios[index]}" é inválido.`);
+      }
+
+      const ano = tempo_contribuicao_mes[index].substring(0, 4);
+      const mes = tempo_contribuicao_mes[index].substring(5, 7);
+      // Faça as validações necessárias para o ano e mês
+    }
+
+    const sql_simulacao = `INSERT INTO simulacao_beneficio (genero, data_nascimento, idade, tempo_contribuicao_mes, idade_aposentadoria) VALUES (?, ?, ?, ?, ?);`;
+    let values = [genero, dataNascimento, idade, tempo_contribuicao_mes && tempo_contribuicao_mes.length, idadeAposentadoria];
+    let [result] = await db.poolSimule.query(sql_simulacao, values);
+    const id_simulacao = result.insertId;
+
+    for (let index = 0; tempo_contribuicao_mes && index < tempo_contribuicao_mes.length; index++) {
+      const salario = salarios[index].replace(".", "").replace("R$", "").replace(",", ".");
+
+      const ano = tempo_contribuicao_mes[index].substring(0, 4);
+      const mes = tempo_contribuicao_mes[index].substring(5, 7);
+
+      const query = `INSERT INTO simulacao_periodo_trabalho (ano, mes, salario, simulacao_beneficio_id) VALUES (?, ?, ?, ?)`;
+      const values = [ano, mes, salario, id_simulacao];
+      [result] = await db.poolSimule.query(query, values);
+    }
+
+    res.send();
+  } catch (error) {
+    // Trate a exceção aqui, você pode enviar uma resposta de erro adequada ao cliente ou fazer outras ações necessárias
+    console.error(error);
+    res.status(500).send('Ocorreu um erro durante a simulação. Por favor, tente novamente mais tarde.');
+  }
+});
+
 module.exports = router;
+
+
+
+
 
--- routes/simuleRouter.js
+++ routes/simuleRouter.js
@@ -55,6 +55,137 @@ router.post('/simule', async function (req, res, next) {
   res.send(renderedResultados);
 });
 
+/*module.exports = router; 
+
+const express = require('express');
+const router = express.Router();
+const db = require('../config/database.config');
+const path = require('path');
+const ejs = require('ejs');
+
+// Função para calcular a idade com base na data de nascimento
+function calcularIdade(dataNascimento) {
+  const hoje = new Date();
+  const nascimento = new Date(dataNascimento);
+  let idade = hoje.getFullYear() - nascimento.getFullYear();
+  const mesAtual = hoje.getMonth();
+  const mesNascimento = nascimento.getMonth();
+  if (mesAtual < mesNascimento || (mesAtual === mesNascimento && hoje.getDate() < nascimento.getDate())) {
+    idade--;
+  }
+  return idade;
+} */
+
+// Função para calcular o mês e ano da aposentadoria
+/* function calcularAposentadoria(idade, genero) {
+  const idadeAposentadoria = genero === 'Masculino' ? 65 : 62;
+  const hoje = new Date();
+  const anoAtual = hoje.getFullYear();
+  const mesAtual = hoje.getMonth() + 1;
+  let mesAposentadoria, anoAposentadoria;
+
+  if (mesAtual <= 6) {
+    mesAposentadoria = 7;
+    anoAposentadoria = anoAtual + idadeAposentadoria - idade;
+  } else {
+    mesAposentadoria = 7;
+    anoAposentadoria = anoAtual + idadeAposentadoria - idade + 1;
+  }
+
+  return {
+    mesAposentadoria,
+    anoAposentadoria
+  };
+}
+
+// Rota para realizar a soma dos valores do campo salario_atualizado no bd previdenciaaquidb:
+app.get('/soma', (req, res) => {
+  const query = 'SELECT SUM(salario_atualizado) AS total FROM simulacao_periodo_trabalho';
+  connection.query(query, (error, results, fields) => {
+    if (error) {
+      console.error('Erro ao executar a consulta:', error);
+      return res.status(500).json({ error: 'Erro ao executar a consulta.' });
+    }
+
+    const total = results[0].total;
+    res.json({ total });
+  });
+});
+
+
+
+// Função para calcular o valor_beneficio
+function calcularBeneficio(genero, idade, salario) {
+  let idadeMinima;
+  let indiceBase;
+  
+  if (genero === 'f') {
+    idadeMinima = 62;
+    if (salario < 180) {
+      return 'Tempo de contribuição abaixo do mínimo requerido!';
+    }
+  } else if (genero === 'm') {
+    idadeMinima = 65;
+    if (salario < 240) {
+      return 'Tempo de contribuição abaixo do mínimo requerido!';
+    }
+  } else {
+    return 'Condição mínima ainda não adquirida!';
+  }
+  
+  if (idade < idadeMinima) {
+    return 'Idade abaixo do mínimo requerido!';
+  }
+  
+  if ((genero === 'f' && salario === 180) || (genero === 'm' && salario === 240)) {
+    indiceBase = 0.6;
+  } else if ((genero === 'f' && salario > 180) || (genero === 'm' && salario > 240)) {
+    const quantidadesExcedentes = (salario - (genero === 'f' ? 180 : 240)) / 12;
+    indiceBase = 0.6 + quantidadesExcedentes * 0.02;
+  }
+
+  const valorBeneficio = total * indiceBase;
+  return valorBeneficio;
+}
+
+
+
+// Rota para exibir o formulário de simulação
+router.get('/simule', function (req, res, next) {
+  res.render('simule');
+});
+
+// Rota para criar uma nova simulação
+router.post('/simule', async function (req, res, next) {
+  let {
+    mesAno,
+    salario,
+    genero,
+    dataNascimento
+  } = req.body;
+
+  genero = genero == "Feminino" ? "f" : "m";
+  const idade = calcularIdade(dataNascimento);
+  const { mesAposentadoria, anoAposentadoria } = calcularAposentadoria(idade, genero);
+
+  const sql_simulacao = `INSERT INTO simulacao_beneficio (genero, data_nascimento, idade, tempo_contribuicao_mes, idade_aposentadoria, mes_aposentadoria, ano_aposentadoria) VALUES (?, ?, ?, ?, ?, ?, ?);`;
+  let values = [genero, dataNascimento, idade, mesAno.length, idadeAposentadoria, mesAposentadoria, anoAposentadoria];
+  let [result] = await db.poolSimule.query(sql_simulacao, values);
+  const id_simulacao = result.insertId;
+
+  for (let index = 0; index < mesAno.length; index++) {
+    const query = `INSERT INTO simulacao_periodo_trabalho (ano, mes, salario, simulacao_beneficio_id) VALUES (?, ?, ?, ?)`;
+    const [ano, mes] = mesAno[index].split('/');
+    const salario_atualizado = parseFloat(salario[index].replace(".", "").replace("R$", "").replace(",", ".")) * obterIndiceAtualizacao(mes);
+
+    const queryValues = [ano, mes, salario_atualizado, id_simulacao];
+    await db.poolSimule.query(query, queryValues);
+  }
+  
+
+  res.redirect('/resultados');
+});
+
 module.exports = router; */
 
 const express = require('express');
@@ -98,91 +229,101 @@ function calcularAposentadoria(idade, genero) {
   };
 }
 
-// Rota para realizar a soma dos valores do campo salario_atualizado
-app.get('/soma', (req, res) => {
-  const query = 'SELECT SUM(salario_atualizado) AS total FROM simulacao_periodo_trabalho';
-  connection.query(query, (error, results, fields) => {
-    if (error) {
-      console.error('Erro ao executar a consulta:', error);
-      return res.status(500).json({ error: 'Erro ao executar a consulta.' });
-    }
-
-    const total = results[0].total;
-    res.json({ total });
-  });
-});
-
-
-
 // Função para calcular o valor_beneficio
-function calcularBeneficio(genero, idade, salarioAtualizado, quantidadeSalarios) {
-  let salario;
-  let idade;
-  
+async function calcularBeneficio(genero, idade, id_simulacao) {
+  let idadeMinima;
+  let indiceBase;
+
   if (genero === 'f') {
-    salario = 180;
-    idade = 62;
+    idadeMinima = 62;
   } else if (genero === 'm') {
-    salario = 240;
-    idade = 65;
+    idadeMinima = 65;
   } else {
-    return 'Sexo inválido!';
+    return 'Condição mínima ainda não adquirida!';
   }
-  
-  if (salario < 180) {
-    return 'Tempo de contribuição abaixo do mínimo requerido!';
-  }
-  
-  if (idade < 62) {
+
+  if (idade < idadeMinima) {
     return 'Idade abaixo do mínimo requerido!';
   }
-  
-  let indiceBase = 0.6;
-  const salario = 180 && genero = f = Math.max(salarioAtualizado - salarioMinimo, 0);
-  const quantidadesExcedentes = Math.floor(salarioExcedente / 12);
-  const indice = indiceBase + quantidadesExcedentes * 0.02;
-  
-  const valorBeneficio = (salarioAtualizado * quantidadeSalarios) / (12 * indice);
-  return valorBeneficio;
+
+  const query = `SELECT SUM(salario_atualizado) as total FROM simulacao_periodo_trabalho WHERE simulacao_beneficio_id = ?`;
+  const [result] = await db.poolSimule.query(query, [id_simulacao]);
+  const salarioTotal = result[0].total;
+
+  if (genero === 'f') {
+    if (salarioTotal < 180) {
+      return 'Tempo de contribuição abaixo do mínimo requerido!';
+    } else if (salarioTotal === 180) {
+      indiceBase = 0.6;
+    } else if (salarioTotal > 180) {
+      const quantidadesExcedentes = (salarioTotal - 180) / 12;
+      indiceBase = 0.6 + quantidadesExcedentes * 0.02;
+    }
+  } else if (genero === 'm') {
+    if (salarioTotal < 240) {
+      return 'Tempo de contribuição abaixo do mínimo requerido!';
+    } else if (salarioTotal === 240) {
+      indiceBase = 0.6;
+    } else if (salarioTotal > 240) {
+      const quantidadesExcedentes = (salarioTotal - 240) / 12;
+      indiceBase = 0.6 + quantidadesExcedentes * 0.02;
+    }
+  }
+
+  const valorBeneficio = salarioTotal * indiceBase;
+  return valorBeneficio.toFixed(2);
 }
 
+// Rota para calcular a soma dos salários atualizados
+router.get('/soma', async (req, res) => {
+  try {
+    const query = `SELECT SUM(salario_atualizado) as total FROM simulacao_periodo_trabalho`;
+    const [result] = await db.poolSimule.query(query);
+    const totalSalarios = result[0].total || 0;
 
-
-// Rota para exibir o formulário de simulação
-router.get('/simule', function (req, res, next) {
-  res.render('simule');
+    res.send(`Total dos salários atualizados: ${totalSalarios}`);
+  } catch (error) {
+    console.error('Erro ao calcular a soma dos salários:', error);
+    res.status(500).send('Erro ao calcular a soma dos salários');
+  }
 });
 
-// Rota para criar uma nova simulação
-router.post('/simule', async function (req, res, next) {
-  let {
-    mesAno,
-    salario,
-    genero,
-    dataNascimento
-  } = req.body;
-
-  genero = genero == "Feminino" ? "f" : "m";
+// Rota para realizar a simulação
+router.post('/simule', async (req, res) => {
+  const { genero, dataNascimento, mesAno, salario } = req.body;
   const idade = calcularIdade(dataNascimento);
   const { mesAposentadoria, anoAposentadoria } = calcularAposentadoria(idade, genero);
 
-  const sql_simulacao = `INSERT INTO simulacao_beneficio (genero, data_nascimento, idade, tempo_contribuicao_mes, idade_aposentadoria, mes_aposentadoria, ano_aposentadoria) VALUES (?, ?, ?, ?, ?, ?, ?);`;
-  let values = [genero, dataNascimento, idade, mesAno.length, idadeAposentadoria, mesAposentadoria, anoAposentadoria];
-  let [result] = await db.poolSimule.query(sql_simulacao, values);
-  const id_simulacao = result.insertId;
+  const sql_simulacao = `INSERT INTO simulacao_beneficio (genero, data_nascimento, idade, tempo_contribuicao_mes, idade_aposentadoria, mes_aposentadoria, ano_aposentadoria) VALUES (?, ?, ?, ?, ?, ?, ?)`;
+  const values = [genero, dataNascimento, idade, mesAno.length, idadeAposentadoria, mesAposentadoria, anoAposentadoria];
 
-  for (let index = 0; index < mesAno.length; index++) {
-    const query = `INSERT INTO simulacao_periodo_trabalho (ano, mes, salario, simulacao_beneficio_id) VALUES (?, ?, ?, ?)`;
-    const [ano, mes] = mesAno[index].split('/');
-    const salario_atualizado = parseFloat(salario[index].replace(".", "").replace("R$", "").replace(",", ".")) * obterIndiceAtualizacao(mes);
+  try {
+    const [result] = await db.poolSimule.query(sql_simulacao, values);
+    const id_simulacao = result.insertId;
 
-    const queryValues = [ano, mes, salario_atualizado, id_simulacao];
-    await db.poolSimule.query(query, queryValues);
+    for (let index = 0; index < mesAno.length; index++) {
+      const query = `SELECT salario_atualizado FROM simulacao_periodo_trabalho WHERE ano = ? AND mes = ? AND simulacao_beneficio_id = ?`;
+      const [ano, mes] = mesAno[index].split('/');
+
+      // Executar a consulta para obter o salario_atualizado
+      const [salarioResult] = await db.poolSimule.query(query, [ano, mes, id_simulacao]);
+      const salario_atualizado = salarioResult[0].salario_atualizado;
+
+      const queryValues = [ano, mes, salario_atualizado, id_simulacao];
+      await db.poolSimule.query(query, queryValues);
+    }
+
+    const valorBeneficio = await calcularBeneficio(genero, idade, id_simulacao);
+
+    res.render(path.resolve(__dirname, '../views/resultados'), {
+      valorBeneficio
+    });
+  } catch (error) {
+    console.error('Erro ao realizar a simulação:', error);
+    res.status(500).send('Erro ao realizar a simulação');
   }
-  
-
-  res.redirect('/resultados');
 });
 
 module.exports = router;
+
 
